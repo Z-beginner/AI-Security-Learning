571fd342571d85ee2df351af33316675b46477c0
Z-beginner <hxh080523@gmail>
2026-08-17 20:27:30 +0800

    alien_invasion

--- 26Summer_Learn/26Summer_Learn2/alien_invasion/alien_invasion.py
+++ 26Summer_Learn/26Summer_Learn2/alien_invasion/alien_invasion.py
@@ -14,15 +14,42 @@ class AlienInvasion:
             (self.settings.screen_width, self.settings.screen_height)) #创建游戏窗口
         pygame.display.set_caption("Alien Invasion") #设置窗口标题
         self.ship = Ship(self)
+
     def run_game(self):
         while True:
-            for event in pygame.event.get(): #获取玩家产生的所有事件
-                if event.type == pygame.QUIT: #玩家退出
-                    sys.exit()
-            self.screen.fill(self.settings.bg_color)  #填充屏幕
-            self.ship.blitme()
-            pygame.display.flip() #更新屏幕
-            self.clock.tick(60) #tick:跳动一次，限制不超过60帧，给循环限速
+            self._check_events()
+            self._update_screen()
+            self.ship.update()
+            self.clock.tick(60)  # tick:跳动一次，限制不超过60帧，给循环限速
+
+    def _check_events(self): #check前的_表示仅供类内部调用
+        for event in pygame.event.get():  # 获取玩家产生的所有事件
+            if event.type == pygame.QUIT:  # 玩家退出
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+    def _check_keyup_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
+    def _update_screen(self):
+        self.screen.fill(self.settings.bg_color)  # 填充屏幕
+        self.ship.blitme()
+        pygame.display.flip()  # 更新屏幕
+
+
 if __name__ == "__main__": #打开python直接运行游戏
     ai = AlienInvasion()
     ai.run_game()
--- 26Summer_Learn/26Summer_Learn2/alien_invasion/ship.py
+++ 26Summer_Learn/26Summer_Learn2/alien_invasion/ship.py
@@ -4,9 +4,21 @@ class Ship:
     def __init__(self, ai_game):
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect() #get.rect()获取矩形框
+        self.settings = ai_game.settings
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
         self.rect.midbottom = self.screen_rect.midbottom  #midbottom:底部中点
+        self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0: # 0 = self.screen_rect.left
+            self.x -= self.settings.ship_speed
+        self.rect.x = self.x
 
     def blitme(self):
         self.screen.blit(self.image, self.rect) #把image绘制到screen的rect上
